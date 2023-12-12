4ec92a8a8d551daca8f32d1ae8809f73c14aa0db
Randle-lanre <kennyrandle09@hotmail.co.uk>
2023-12-12 20:21:22 +0000

    added initial type converter

--- DtoTransfer/Program.cs
+++ DtoTransfer/Program.cs
@@ -7,8 +7,8 @@ namespace DtoTransfer;
 
 internal class Program
 {
+   static string basePath = @"C:\Users\Kehinde\RiderProjects\DtoTransfer\DtoTransfer\TestDtoOutput";
 
-    
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
@@ -16,37 +16,74 @@ internal class Program
         var assembly = Assembly.GetExecutingAssembly();
         var dtoClasses = assembly.GetTypes().Where(type => type.Name.EndsWith("Dto")).ToList();
 
-        // FileHelper();
-        
         foreach (var dto in dtoClasses)
         {
-            Console.WriteLine("class Name ---> {0}",dto.Name);
+            Console.WriteLine("class Name ---> {0}", dto.Name); //pass to File create
+
+            #region Create_Type_File
+
+            var ( _, typeFileCreated )= FileHelper(dto.Name);
+
+            #endregion
 
             var propties = dto.GetProperties().ToList();
 
             foreach (PropertyInfo item in propties)
             {
-                Console.WriteLine("PropertyName --> {0}, PropertyType --> {1}",item.Name, item.PropertyType);
-                
+                #region Write_To_Type_File
+
+                if (!string.IsNullOrWhiteSpace(typeFileCreated))
+                {
+                    //
+                    FileWriter(item.Name.ToString(), item.PropertyType.ToString(), typeFileCreated);
+                }
+
+                #endregion
+
+                Console.WriteLine("PropertyName --> {0}, PropertyType --> {1}", item.Name, item.PropertyType);
             }
-
         }
-
     }
 
 
-    static void FileHelper(string dtoName)
+    static (bool, string) FileHelper(string dtoName)
     {
-        var basePath = @"C:\Users\Kehinde\RiderProjects\DtoTransfer\DtoTransfer\TestDtoOutput";
+        if (string.IsNullOrWhiteSpace(dtoName)) return (false, "");
+        File.Create($"{basePath}\\{dtoName}.ts");
+        return (true, $"{basePath}\\{dtoName}.ts");
 
+    }
 
+    static bool TypePrefix(string filePath, string dtoName)
+    {
+        //export Interface name {
 
-        if (!string.IsNullOrWhiteSpace(dtoName))
+        return true;
+    }
+
+    static bool TypeSuffix(string filePath, string dtoName)
+    {
+        // }
+        return true;
+    }
+
+    static void FileWriter(string propName, string propType, string typeFile)
+    {
+        if (File.Exists(typeFile))
         {
-            File.Create($"{basePath}\\{dtoName}.ts");
+            
         }
-        
+    
+        //
         
     }
-    
+
+    static string TypeConverter(string propType) => propType switch
+    {
+        "System.Int32" => "number",
+        "System.String" => "string",
+        "System.DateTime" => "Datetime",
+        "System.Double" => "float"
+    };
+
 }
