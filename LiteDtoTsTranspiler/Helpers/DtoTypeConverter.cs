00b944e4af9d9c16a0a70b7b1a985a073241d3a6
Randle-lanre <kennyrandle09@hotmail.co.uk>
2023-12-27 22:19:20 +0000

    included more types

--- LiteDtoTsTranspiler/Helpers/DtoTypeConverter.cs
+++ LiteDtoTsTranspiler/Helpers/DtoTypeConverter.cs
@@ -4,12 +4,14 @@ public class DtoTypeConverter
 {
     public string ConvertCsTypeToTsType(string propType) => propType switch
     {
-        "System.Int32" => "number",
+        "System.Int32" or "System.Double" or "System.UInt64" or "System.UInt16" or "System.UInt32"
+            or "System.Int16" => "number",
         "System.String" => "string",
         "System.DateTime" => "Date",
-        "System.Double" => "number",
         "System.Int64" => "bigint",
         "System.Boolean" => "boolean",
+        "System.Object" => "any",
+        "System.Enum" => "string",
         _ => "any"
     };
 }
